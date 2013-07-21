82f1b0e6e6a8b5cade13676e52f0282eb37c09d3
Bosse Andersson <bosse56@gmail.com>
2013-07-21 13:24:01 +0000

    update to web.js, text from file with use of Buffer

--- web.js
+++ web.js
@@ -4,7 +4,8 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var outtext = fs.readFileSync('index.html', 'utf-8');
+var text = new Buffer(fs.readFileSync('index.html'));
+var outtext = text.toString('utf-8');
 
 app.get('/', function(request, response) {  
   response.send(outtext);
