29a0b2e3c381bdce9e129414bcc7955e14c2a492
Ubuntu <ubuntu@ip-172-31-34-239.us-west-2.compute.internal>
2013-08-07 19:15:27 +0000

    New index.html, copied after course instruction

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
 var express = require('express');
 
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-var text = new Buffer(fs.readFileSync('index.html'));
-var outtext = text.toString('utf-8');
-
-app.get('/', function(request, response) {  
-  response.send(outtext);
+app.get('/', function(request, response) {
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
